fa32549a965dc0a09f018cf2738aa6c2915f52db
akıncı08003 <ia7468049@gmail.com>
2025-12-17 21:20:30 +0300

    Add files via upload

---
+++ ESE4.5kod.ino
@@ -0,0 +1,71 @@
+#include <LiquidCrystal.h>
+
+#define buton1 2
+#define buton2 3
+#define kirmiziled 6 
+#define yesilled 7
+LiquidCrystal lcd (13,12,11,10,9,8);
+void setup() {
+lcd.begin(16,2);
+
+pinMode(buton1 ,INPUT_PULLUP);
+pinMode(buton2 ,INPUT_PULLUP);
+pinMode(kirmiziled,OUTPUT);
+pinMode (yesilled,OUTPUT);  
+
+}
+
+double bartis=29.5;
+double bazalis=28.5;
+
+void loop() {
+  double aralikkucuk=10*bazalis;
+  double aralikbuyuk=10*bartis;
+  double sicaklik =random (aralikkucuk,aralikbuyuk)/10.0;
+  butonkontrolleri( sicaklik);
+  lcdveled( sicaklik );
+}
+void butonkontrolleri (double sicaklik )
+{
+  double aralikkucuk=10*bazalis;
+  double aralikbuyuk=10*bartis;
+  sicaklik =random (aralikkucuk,aralikbuyuk)/10.0;
+  if (digitalRead(buton1) ==LOW) 
+  {
+    bartis=bartis+1;
+    bazalis=bazalis+1;
+    sicaklik =random (aralikkucuk,aralikbuyuk)/10.0;
+    delay(100);
+  }
+   if (digitalRead(buton2) == LOW) {
+    bartis = bartis - 1;
+    bazalis = bazalis - 1;
+    sicaklik = random(aralikkucuk, aralikbuyuk) / 10.0;
+    delay(100);
+  }
+}
+void lcdveled(double sicaklik)
+{
+ 
+  lcd.setCursor(0,0);
+  lcd.print("aralik:");
+  lcd.print(bazalis,1);
+  lcd.print("-");
+  lcd.print(bartis,1);
+
+  lcd.setCursor(0,1);
+  lcd.print("sicaklik : ");
+  lcd.print(sicaklik);
+ 
+    if(sicaklik>=25)
+    {   
+      digitalWrite(kirmiziled,HIGH);
+      digitalWrite(yesilled,LOW);
+    }
+    else
+    {
+      digitalWrite(yesilled,HIGH);
+      digitalWrite(kirmiziled,LOW);
+      
+    }
+}
